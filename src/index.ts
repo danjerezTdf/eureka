3844f5b0e46a82656bb8cab2829a15aa3507442c
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:24:57 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+/* Eureka
+• Escribir un algoritmo que nos pida
+una clave y verifique que sea la
+correcta
+• Tenga en cuenta que la clave es la
+palabra “eureka”
+• Solo tenemos 3 intentos para
+acertar, si fallamos los 3 intentos
+el sistema mostrará un mensaje
+indicándonos que hemos agotado
+todas las oportunidades
+• Si acertamos la clave, saldremos
+directamente del programa */
+
 let claveGuardada: string = "eureka";
 let contador: number = 0;
 let claveIngresada: string | null = String(prompt(`Ingreser clave:`));
